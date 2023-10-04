cbba1f9f8dfbf29efbf5539fe546730a35f63f1d
Linna <93605506+LinnaAlice@users.noreply.github.com>
2023-10-04 18:06:00 +0200

    Added dots flocking behavior

--- main.js
+++ main.js
@@ -9,14 +9,164 @@ function preload() {
 function setup() {
   createCanvas(innerWidth, innerHeight);
   background(255);
+
+  for (let i = 0; i < random(150, 500); i++) {
+    flock.push(new Dot());
+  }
 }
 
-function draw() {}
+function draw() {
+  for (let dot of flock) {
+    dot.edges();
+    dot.flock(flock);
+    dot.update();
+    dot.display();
+  }
+}
 
 class Dot {
-  constructor() {}
+  constructor() {
+    this.pos = createVector(random(width), random(height));
+    // Gives you a random velocity vector (no hard coded values) of unit length 1
+    this.vel = p5.Vector.random2D();
+    // The velocity is a random value between 0.5 and 3 (speed)
+    this.vel.setMag(random(0.5, 3));
+    this.acc = createVector();
+    this.maxForce = 0.4;
+    this.maxSpeed = 2;
+  }
 
-  update() {}
+  edges() {
+    if (this.pos.x > innerWidth) {
+      this.pos.x = 0;
+    } else if (this.pos.x < 0) {
+      this.pos.x = innerWidth;
+    }
+    if (this.pos.y > innerHeight) {
+      this.pos.y = 0;
+    } else if (this.pos.y < 0) {
+      this.pos.y = innerHeight;
+    }
+  }
 
-  display() {}
+  // This function align this dot with all other dots
+  // Getting the average velocity of all the dots within a certain radius
+  align(dots) {
+    let perceptionRadius = 50;
+    let steering = createVector();
+    let total = 0;
+
+    for (let surroundingDots of dots) {
+      let distance = dist(this.pos.x, this.pos.y, surroundingDots.pos.x, surroundingDots.pos.y);
+      // As long as other isn't me and the distance is less than 100, add it up and divide by the total
+      if (distance < perceptionRadius && surroundingDots != this) {
+        steering.add(surroundingDots.vel);
+        total++;
+      }
+    }
+
+    // I only want to divide by the total if total is greate than 0
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(this.maxSpeed);
+      // Steering = Desired velocity - Velocity
+      steering.sub(this.vel);
+      // Limits the magnitude of a vector
+      steering.limit(this.maxForce);
+    }
+
+    return steering;
+  }
+
+  // Same as align, BUT we are subtracting this.pos
+  cohesion(dots) {
+    let perceptionRadius = 100;
+    let steering = createVector();
+    let total = 0;
+
+    for (let surroundingDots of dots) {
+      let distance = dist(this.pos.x, this.pos.y, surroundingDots.pos.x, surroundingDots.pos.y);
+      // As long as other dot isn't me and the distance is less than 100, add it up and divide by the total
+      if (distance < perceptionRadius && surroundingDots != this) {
+        // Adding up the velocities of any surrounding dot
+        steering.add(surroundingDots.pos);
+        total++;
+      }
+    }
+
+    // I only want to divide by the total if total is greater than 0
+    if (total > 0) {
+      steering.div(total);
+      steering.sub(this.pos);
+      steering.setMag(this.maxSpeed);
+      // Steering = Desired velocity - Velocity
+      steering.sub(this.vel);
+      steering.limit(this.maxForce);
+    }
+
+    return steering;
+  }
+
+  separation(dots) {
+    let perceptionRadius = 50;
+    let steering = createVector();
+    let total = 0;
+
+    for (let surroundingDots of dots) {
+      let distance = dist(this.pos.x, this.pos.y, surroundingDots.pos.x, surroundingDots.pos.y);
+      // As long as other isn't me and the distance is less than 100, add it up and divide by the total
+      if (distance < perceptionRadius && surroundingDots != this) {
+        let diff = p5.Vector.sub(this.pos, surroundingDots.pos);
+        diff.div(distance);
+        steering.add(diff);
+        total++;
+      }
+    }
+
+    // I only want to divide by the total if total is greate than 0
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(this.maxSpeed);
+      // Steering = Desired velocity - Velocity
+      steering.sub(this.vel);
+      steering.limit(this.maxForce);
+    }
+
+    // Always return a vector or 0 if none
+    return steering;
+  }
+
+  flock(dots) {
+    // Force accumulation: Having both alignment, cohesion and separation
+    this.acc.set(0, 0); // Or mult(0);
+    let alignment = this.align(dots);
+    let cohesion = this.cohesion(dots);
+    let separation = this.separation(dots);
+
+    alignment.mult(alignSlider.value());
+    cohesion.mult(cohesionSlider.value());
+    separation.mult(separationSlider.value());
+
+    // Add them together to get net force 0
+    this.acc.add(alignment);
+    this.acc.add(cohesion);
+    this.acc.add(separation);
+  }
+
+  update() {
+    // The position is updated based on the velocity
+    this.pos.add(this.vel);
+    // The velocity is updated based on the acceleration
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+  }
+
+  display() {
+    push();
+    noStroke();
+    let col = img.get(random(this.pos.x), random(this.pos.y));
+    fill(col);
+    ellipse(this.pos.x, this.pos.y, dotSize);
+    pop();
+  }
 }
